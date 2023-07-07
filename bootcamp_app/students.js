40a38a5e96a824621aec6ff87a55cc0841679994
Vicky John <vicky.d.john@email.com>
2023-07-06 23:32:04 -0700

    added queries using node-postgres for students and teachers

---
+++ bootcamp_app/students.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const input = process.argv.slice(2);
+const cohortName = input[0];
+const limit = input[1];
+pool.query(`
+  SELECT students.id, students.name, cohorts.name AS cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE '${cohortName}%'
+  LIMIT ${limit};
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    });
+  })
+  .catch(err => {
+    console.error('Error:', err);
+  });
